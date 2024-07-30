4994444d8d0ecc4bdf3a18806f33fca91322b40e
Fayikk <yakupaktar99@gmail.com>
2024-07-30 17:41:24 +0300

    api bağlantıları yapıldı

--- Apis/categoryApi.js
+++ Apis/categoryApi.js
@@ -5,5 +5,31 @@ export const categoryApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "",
   }),
-  endpoints: () => ({}),
+  endpoints: (builder) => ({
+    GetAllCategory: builder.query({
+      query: () => ({
+        url: "Category",
+        method: "GET",
+      }),
+    }),
+    CreateCategory: builder.mutation({
+      query: (categoryModel) => ({
+        url: "Category",
+        method: "POST",
+        body: categoryModel,
+      }),
+    }),
+    RemoveCategory: builder.mutation({
+      query: (categoryId) => ({
+        url: `Category/${categoryId}`,
+        method: "DELETE",
+      }),
+    }),
+    UpdateCategory: builder.mutation({
+      query:(model)=>({
+        url:
+      }),
+      query:
+    }),
+  }),
 });
