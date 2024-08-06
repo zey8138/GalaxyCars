988a7ed3799ae6e1f3829e40551a112ba5871d22
Fayikk <yakupaktar99@gmail.com>
2024-08-06 13:45:03 +0300

    .d

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./Storage/store";
-import TryScreen from "./Screens/tryScreen";
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
         <View style={styles.container}>
-          <TryScreen></TryScreen>
+          <Text>Hello My Galaxy</Text>
           <StatusBar style="auto" />
         </View>
       </Provider>
