4d8381cb96380d56d9420d8ed5cd3bdee3a2da80
Zeynep Yankılınç <86042249+zey8138@users.noreply.github.com>
2024-08-08 22:18:36 +0300

    initial commit

--- App.js
+++ App.js
@@ -2,8 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./Storage/store";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 export default function App() {
+
+  const Drawer = createDrawerNavigator();
+
+  function CategoryOverview(){
+    return (
+      <Drawer.Navigator initialRouteName='Category' >
+        <Drawer.Screen name='Kategoriler' component={Category} ></Drawer.Screen>
+
+      </Drawer.Navigator>
+    )
+  }
+
+
+
   return (
     <>
       <Provider store={store}>
